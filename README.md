ae0f0c7d10dd9b76f7f59c7662254a0226311ce4
Shawn Rider <shawn@shawnrider.com>
2015-04-11 16:14:20 -0700

    Updated assignment.
    Change-Id: I7a0fda0c71ada695a73a02d885800a098b0370dd

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 # wats1020-dom-basics
-In this assignment students explore the native tools provided in Javascript to navigate and affect the DOM.
+In this assignment students explore the native tools provided in Javascript to
+navigate and affect the DOM. To practice these techniques, we will createa
+Fortune Cookie Generator page that will show you a saying like you might find
+in a fortune cookie.
 
- PLAN: Assignment contains a simple HTML/css setup with several areas where data should be that are empty. Students have to write Javascript that is loaded at the end of the file to fill in the blank spots with information provided in an Array. This is a simple simulation of getting data from an API and dumping it into the page, and introduces the basics of finding elements in the DOM and adding information into them. This uses all raw Javascript methods.
+Feel free to have fun with this, and there are lots of ways to stretch in this
+assignment. If done right, this could be a fun portfolio piece.
+
+## Basic Requirements
+To complete the base level of this assignment, you must fulfill the following
+requirements:
+
+* Add an `onclick` attribute to the "Make My Fortune!" button so it executes the `generateFortuneCookie()` function when it is clicked.
+* In the `fortune-cookie-generator.js` create the logic to display a fortune:
+    * Select the `#fortune-cookie-text` element.
+    * Append the current text in the `#fortune-cookie-text` element as a list item (`li`) within the `#previous-fortune-cookies` list.
+    * Select a random fortune cookie saying from the `fortunesList` Array.
+    * Replace the `innerText` of the `#fortune-cookie-text` element with the data you selected from `fortunesList`.
+* Make sure a fortune is generated each time the user clicks the button.
+
+## Stretch Goals
+This assignment allows for a large number of possible stretch goals. You are encouraged to stretch in additional ways if you can think of fun/interesting things to try. Here are some ideas:
+
+* It's currently possible to randomly select the same fortune twice in a row. Can you think of a way to check if the new saying is the same as the previous saying and prevent it from showing up? Can you think of a way to keep track of all the sayings that have been used and prevent a saying from being repeated until the entire list has been exhausted?
+* This page is begging for some cute visuals. Use CSS to style the elements in the HTML. Feel free to add or rearrange HTML elements to fit your vision.
+* You could show additional statistics on the page, such as how many fortunes have been generated. Add those in an appealing way.
+* What if people want to generate fortunes for their whole table? Why not allow somebody to generate multiple fortunes at the same time? How would you change the code and HTML to support that?
---
+++ fortune-cookie-generator.js
@@ -0,0 +1,84 @@
+//////////////////////////////////////////////////////////////////
+//
+// Fortune Cookie Generator
+//
+//////////////////////////////////////////
+
+var generateFortuneCookie = function() {
+    // This is where your code for the Fortune Cookie generator goes.
+    // You will use the fortunesList variable defined lower in this file
+    // to supply your fortune cookies with text.
+
+    // TODO: Grab the paragraph with the ID
+    // `fortune-cookie-text` to be able to insert text into that element.
+
+    // TODO: Update the Previous Fortunes list with the current `innerText`
+    // value of `#fortune-cookie-text`. Follow these steps:
+        // 1. Create a new `li` element with the `document.createElement()` method.
+        // 2. Set the `innerText` of that element equal to the `innerText` of
+        //    the `#fortune-cookie-text` element.
+        // 3. Select the `#previous-fortune-cookies` container and use
+        //    `appendChild()` to append the new `li` element you created above.
+        // 4. You should see the previous fortune cookie saying show up in the list.
+
+    // TODO: Select a new (random) fortune cookie saying from the data stored in the
+    // `fortunesList` variable. (HINT: You will use `Math.floor()` and
+    // `Math.random()` to accomplish this.) Use this data to update the
+    // `innerText` of the `#fortune-cookie-text` element.
+
+}
+
+// The following data list is provided for you to use in your code.
+var fortunesList = [
+    "People are naturally attracted to you.",
+    "You learn from your mistakes... You will learn a lot today.",
+    "If you have something good in your life, don't let it go!",
+    "What ever you're goal is in life, embrace it visualize it, and for it will be yours.",
+    "Your shoes will make you happy today.",
+    "You cannot love life until you live the life you love.",
+    "Be on the lookout for coming events; They cast their shadows beforehand.",
+    "Land is always on the mind of a flying bird.",
+    "The man or woman you desire feels the same about you.",
+    "Meeting adversity well is the source of your strength.",
+    "A dream you have will come true.",
+    "Our deeds determine us, as much as we determine our deeds.",
+    "Never give up. You're not a failure if you don't give up.",
+    "You will become great if you believe in yourself.",
+    "There is no greater pleasure than seeing your loved ones prosper.",
+    "You will marry your lover.",
+    "The greatest love is self-love.",
+    "A very attractive person has a message for you.",
+    "You already know the answer to the questions lingering inside your head.",
+    "It is now, and in this world, that we must live.",
+    "You must try, or hate yourself for not trying.",
+    "You can make your own happiness.",
+    "The greatest risk is not taking one.",
+    "The love of your life is stepping into your planet this summer.",
+    "Love can last a lifetime, if you want it to.",
+    "Adversity is the parent of virtue.",
+    "Serious trouble will bypass you.",
+    "A short stranger will soon enter your life with blessings to share.",
+    "Now is the time to try something new.",
+    "Wealth awaits you very soon.",
+    "If you feel you are right, stand firmly by your convictions.",
+    "If winter comes, can spring be far behind?",
+    "Keep your eye out for someone special.",
+    "You are very talented in many ways.",
+    "A stranger, is a friend you have not spoken to yet.",
+    "A new voyage will fill your life with untold memories.",
+    "You will travel to many exotic places in your lifetime.",
+    "Your ability for accomplishment will follow with success.",
+    "Nothing astonishes men so much as common sense and plain dealing.",
+    "Its amazing how much good you can do if you dont care who gets the credit.",
+    "Everyone agrees. You are the best.",
+    "LIFE CONSISTS NOT IN HOLDING GOOD CARDS, BUT IN PLAYING THOSE YOU HOLD WELL.",
+    "Jealousy doesn't open doors, it closes them!",
+    "It's better to be alone sometimes.",
+    "When fear hurts you, conquer it and defeat it!",
+    "Let the deeds speak.",
+    "You will be called in to fulfill a position of high honor and responsibility.",
+    "The man on the top of the mountain did not fall there.",
+    "You will conquer obstacles to achieve success.",
+    "Joys are often the shadows, cast by sorrows.",
+    "Fortune favors the brave."
+]
